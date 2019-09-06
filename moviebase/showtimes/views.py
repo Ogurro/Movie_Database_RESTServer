320b3fe890637d931f6f8413edd9798a592690c4
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-06 22:40:05 +0200

    add backend filtering for screenings (city and movie)

--- moviebase/showtimes/views.py
+++ moviebase/showtimes/views.py
@@ -1,6 +1,16 @@
 from .models import Cinema, Screening
 from .serializers import CinemaSerializer, ScreeningSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters import rest_framework as filters
+
+
+class ScreeningsFilter(filters.FilterSet):
+    movie = filters.CharFilter(field_name='movie__title', lookup_expr='icontains')
+    city = filters.CharFilter(field_name='cinema__city', lookup_expr='icontains')
+
+    class Meta:
+        model = Screening
+        fields = ['movie', 'city']
 
 
 class CinemaListView(ListCreateAPIView):
@@ -16,6 +26,7 @@ class CinemaView(RetrieveUpdateDestroyAPIView):
 class ScreeningListView(ListCreateAPIView):
     queryset = Screening.objects.all()
     serializer_class = ScreeningSerializer
+    filterset_class = ScreeningsFilter
 
 
 class ScreeningsView(RetrieveUpdateDestroyAPIView):
